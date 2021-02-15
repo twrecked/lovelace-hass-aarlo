27c238a5e1beab451b90c0c752569d4a3f0b20ec
alceasan <3265883+alceasan@users.noreply.github.com>
2021-02-15 10:09:08 +0100

    Create es.js

---
+++ lang/es.js
@@ -0,0 +1,75 @@
+
+export var messages = {
+
+	// maintainer
+	author: {
+		name: "twrecked",
+	},
+
+	// image viewer
+	image: {
+		turn_camera_on:    'encender cámara',
+		turn_camera_off:   'apagar cámara',
+		take_a_snapshot:   'hacer captura',
+		start_stream:      'ver vídeo en directo',
+		stop_stream:       'parar vídeo en directo',
+		automatic_capture: 'capturado automáticamente a las ',
+		snapshot_capture:  'capturado a las ',
+	},
+	
+	// library viewer
+	library: {
+		first_page:    'primera página',
+		previous_page: 'página previa',
+		next_size:     'siguiente tamaño de parrilla',
+		close:         'cerrar archivo',
+		next_page:     'página siguiente,
+		last_page:     'última página',
+		captured:      'capturado',
+	},
+
+	// video/stream viewer
+	video: {
+		stop:       'parar',
+		play:       'reproducir',
+		pause:      'pausar',
+		fullscreen: 'pantalla completa'
+	},
+
+	// device statuses
+	status: {
+		battery_strength:   'carga de batería',
+		detected:           'detectado',
+		door_closed:        'cerrada',
+		door_open:          'abierta',
+		captured:           'capturado',
+		captured_nothing:   'nada hoy',
+		captured_something: 'vídeos hoy, último a las',
+		clear:              'despejado',
+		plugged_in:         'conectado',
+		signal_strength:    'potencia de la señal',
+		motion:             'movimiento',
+		sound:              'sonido',
+		lock_locked:        'bloqueado (click para desbloquear)',
+		lock_unlocked:      'desbloqueado (click para bloquear)',
+		doorbell_pressed:   'ding dong!',
+		doorbell_idle:      'sin actividad',
+		doorbell_mute:      'timbre activo (click para silenciar)',
+		doorbell_muted:     'timbre silenciado (click para activar)',
+		light_on:           'encendida',
+		light_off:          'apagada',
+		next_camera:        'siguiente cámara',
+		previous_camera:    'cámera anterior',
+		library:            'archivo',
+		library_empty:      'no hay grabaciones disponibles',
+		library_open:       'click para abrir',
+	},
+	
+	// known triggers
+	trigger : {
+		animal:  "animal",
+		vehicle: "vehículo",
+		person:  "persona",
+		parcel:  "paquete"
+	},
+}
