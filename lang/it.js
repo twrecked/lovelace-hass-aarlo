4b26c6351b54183866ed04c3dd70ec3dfe646967
Steve Herrell <steve.herrell@gmail.com>
2021-02-17 08:01:51 -0500

    Added Italian and Swedish.
    Thanks to:
       Italian: https://github.com/QuakeGio83
       Swedish: https://github.com/pierrebengtsson

---
+++ lang/it.js
@@ -0,0 +1,74 @@
+export var messages = {
+
+	// maintainer
+	author: {
+		name: "twrecked",
+	},
+
+	// image viewer
+	image: {
+		turn_camera_on:    'accendi la videocamera',
+		turn_camera_off:   'spegni la videocamera',
+		take_a_snapshot:   'cattura una immagine',
+		start_stream:      'avvia la diretta video',
+		stop_stream:       'interrompi la diretta video',
+		automatic_capture: 'cattura automaticamente alle',
+		snapshot_capture:  'cattura una immagine alle',
+	},
+	
+	// library viewer
+	library: {
+		first_page:    'prima pagina',
+		previous_page: 'pagina precedente',
+		next_size:     'dimensione griglia successiva',
+		close:         'chiudi libreria',
+		next_page:     'pagina sussessiva',
+		last_page:     'ultima pagina',
+		captured:      'catturata',
+	},
+
+	// video/stream viewer
+	video: {
+		stop:       'stop',
+		play:       'avanti',
+		pause:      'pausa',
+		fullscreen: 'vai a tutto schermo'
+	},
+
+	// device statuses
+	status: {
+		battery_strength:   'carica della batteria',
+		detected:           'rilevato',
+		door_closed:        'chiusa',
+		door_open:          'aperta',
+		captured:           'catturata',
+		captured_nothing:   'oggi nulla',
+		captured_something: 'video rilevato oggi alle',
+		clear:              'pulito',
+		plugged_in:         'inserito',
+		signal_strength:    'potenza segnale',
+		motion:             'movimento',
+		sound:              'suono',
+		lock_locked:        'bloccato (click per aprire)',
+		lock_unlocked:      'sbloccato (click per bloccare)',
+		doorbell_pressed:   'ding dong!',
+		doorbell_idle:      'inattivo',
+		doorbell_mute:      'attivo (click per silenziare)',
+		doorbell_muted:     'silenzioso (click per riattivare)', 
+		light_on:           'on',
+		light_off:          'off',
+		next_camera:        'videocamera successiva',
+		previous_camera:    'videocamera precedente',
+		library:            'libreria',
+		library_empty:      'nessuna registrazione disponibile',
+		library_open:       'click per aprire',
+	},
+	
+	// known triggers
+	trigger : {
+		animal:  "animale",
+		vehicle: "veicolo",
+		person:  "persona",
+		parcel:  "pacco"
+	},
+}
---
+++ lang/sv.js
@@ -0,0 +1,75 @@
+
+export var messages = {
+
+	// maintainer
+	author: {
+		name: "twrecked",
+	},
+
+	// image viewer
+	image: {
+		turn_camera_on:    'sätt på kameran',
+		turn_camera_off:   'stäng av kameran',
+		take_a_snapshot:   'ta en bild',
+		start_stream:      'starta live-sändning',
+		stop_stream:       'stoppa live-sändning',
+		automatic_capture: 'tagen automatiskt klockan',
+		snapshot_capture:  'bild tagen klockan',
+	},
+	
+	// library viewer
+	library: {
+		first_page:    'första sidan',
+		previous_page: 'föregående sida',
+		next_size:     'nästa rutnätsstorlek',
+		close:         'stäng biblioteket',
+		next_page:     'nästa sida',
+		last_page:     'sista sidan',
+		captured:      'tagen',
+	},
+
+	// video/stream viewer
+	video: {
+		stop:       'stopp',
+		play:       'play',
+		pause:      'pause',
+		fullscreen: 'starta helskärm'
+	},
+
+	// device statuses
+	status: {
+		battery_strength:   'batterinivå',
+		detected:           'upptäckte',
+		door_closed:        'stängd',
+		door_open:          'öppen',
+		captured:           'tagen',
+		captured_nothing:   'inget idag',
+		captured_something: 'filmer idag, senaste klockan',
+		clear:              'fritt',
+		plugged_in:         'inkopplad',
+		signal_strength:    'signalstyrka',
+		motion:             'rörelse',
+		sound:              'ljud',
+		lock_locked:        'låst (klicka för att låsa upp)',
+		lock_unlocked:      'olåst (klicka för att låsa)',
+		doorbell_pressed:   'ding dong!',
+		doorbell_idle:      'standby',
+		doorbell_mute:      'Ljud på (klicka för att stänga av)',
+		doorbell_muted:     'Ljud av (klicka för att sätta på)',
+		light_on:           'på',
+		light_off:          'av',
+		next_camera:        'nästa kamera',
+		previous_camera:    'föregående kamera',
+		library:            'bibliotek',
+		library_empty:      'inga inspelningar tillgängliga',
+		library_open:       'klicka för att öppna',
+	},
+	
+	// known triggers
+	trigger : {
+		animal:  "djur",
+		vehicle: "fordon",
+		person:  "person",
+		parcel:  "paket"
+	},
+}
