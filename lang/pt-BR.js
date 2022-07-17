fc026e5606a916d1cedc9451bb761623d4e69624
Leandro Issa <67451572+LeandroIssa@users.noreply.github.com>
2022-07-17 09:35:54 -0300

    Add files via upload

---
+++ lang/pt-BR.js
@@ -0,0 +1,92 @@
+
+export var messages = {
+
+	// maintainer
+	author: {
+		name: "twrecked",
+	},
+
+	// image viewer
+	image: {
+		turn_camera_on:    'ligue a câmera',
+		turn_camera_off:   'desligue a câmera',
+		take_a_snapshot:   'tire uma foto',
+		start_stream:      'ver stream',
+		stop_stream:       'parar stream',
+		automatic_capture: 'capturado automaticamente em',
+		snapshot_capture:  'foto capturada em',
+		feature_disabled:  'câmera desligada, recurso desativado'
+	},
+	
+	// library viewer
+	library: {
+		first_page:    'primeira página',
+		previous_page: 'página anterior',
+		next_size:     'próximo tamanho da grade',
+		close:         'fechar biblioteca',
+		next_page:     'próxima página',
+		last_page:     'última página',
+		captured:      'capturada',
+		duration:      'duração',
+		reason:        'razão',
+	},
+
+	// video/stream viewer
+	video: {
+		stop:       'parar',
+		play:       'reproduzir',
+		pause:      'pausar',
+		fullscreen: 'ir em tela cheia'
+	},
+
+	// device statuses
+	status: {
+		battery_strength:   'força da bateria',
+		detected:           'detectou',
+		door_closed:        'fechada',
+		door_open:          'abrir',
+		captured:           'capturada',
+		captured_nothing:   'nada hoje',
+		captured_something: 'clipes hoje, último às',
+		clear:              'limpar',
+		plugged_in:         'conectado',
+		signal_strength:    'sinal de força',
+		motion:             'movimento',
+		sound:              'som',
+		lock_locked:        'bloqueado (clique para desbloquear)',
+		lock_unlocked:      'desbloqueado (clique para bloquear)',
+		doorbell_pressed:   'ding dong!',
+		doorbell_idle:      'ocioso',
+		doorbell_mute:      'sem som (clique para silenciar)',
+		doorbell_muted:     'silenciado (clique para ativar o som)',
+		light_on:           'ligar',
+		light_off:          'desligar',
+		next_camera:        'próxima câmera',
+		previous_camera:    'câmera anterior',
+		library:            'biblioteca',
+		library_empty:      'não há gravações disponíveis',
+		library_open:       'clique para abrir',
+	},
+	
+	// known triggers
+	trigger : {
+		animal:  "animal",
+		vehicle: "veículo",
+		person:  "pessoa",
+		package: "pacote"
+	},
+
+	// camera states
+	state: {
+		off:                "Desligada",
+		idle:               "Ocioso",
+		recording:          "Gravando",
+		streaming:          "Transmitindo",
+		recording_snapshot: "Gravando + foto",
+		streaming_snapshot: "Transmitindo + foto",
+		taking_snapshot:    "Tirar foto",
+		recently_active:    "Recentemente ativo",
+		unavailable:        "Indisponível",
+		offline_too_cold:   "Offline, muito frio",
+	}
+}
