63b713d774ca9d00eb07bfd260d0f4846f9575e4
Rainier Young <wyoung@outlook.com>
2022-11-08 13:04:31 -0600

    base url formatting change

--- dist/hass-aarlo.js
+++ dist/hass-aarlo.js
@@ -1508,8 +1508,8 @@ class AarloGlance extends HTMLElement {
      */
     generateImageURL() {
         const camera = this._getState(this.cc.id,'unknown');
-        this.cs.image = document.baseURI + camera.attributes.entity_picture + "&t=" + new Date().getTime()
-        this.cs.imageBase = document.baseURI + camera.attributes.entity_picture
+        this.cs.image = new URL("", document.baseURI).href + camera.attributes.entity_picture + "&t=" + new Date().getTime()
+        this.cs.imageBase = new URL("", document.baseURI).href + camera.attributes.entity_picture
     }
 
     generateImageURLLater(seconds = 2) {
