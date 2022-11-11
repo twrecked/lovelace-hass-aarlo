7eb9df9e0b0b1773fc35cafe7ffd1256732a1c6a
Rainier Young <wyoung@outlook.com>
2022-11-11 14:53:42 -0600

    per logs imageBase migght have the url

--- dist/hass-aarlo.js
+++ dist/hass-aarlo.js
@@ -1507,7 +1507,7 @@ class AarloGlance extends HTMLElement {
      * It makes no attempt to reload the image.
      */
     generateImageURL() {
-        const instance_url = this._hass.config.external_url
+        const instance_url = this.cs.imageBase
 
         const camera = this._getState(this.cc.id,'unknown');
         this.cs.image = instance_url + camera.attributes.entity_picture + "&t=" + new Date().getTime()
