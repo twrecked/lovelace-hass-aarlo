536a47de291d7939d20f07c6c192662cf3c5a76e
rainieryoung <wyoung@outlook.com>
2022-12-22 08:32:34 -0600

    .id doesn't have state history
    motiionId is current best option

--- dist/hass-aarlo.js
+++ dist/hass-aarlo.js
@@ -1412,7 +1412,7 @@ class AarloGlance extends HTMLElement {
         if (id.startsWith("camera-motion")) {
             this.moreInfo(this.cc.motionId)
         } else if (id.startsWith("camera-statusIcon")) {
-            this.moreInfo(this.cc.id)
+            this.moreInfo(this.cc.motionId)
         } else if (id.startsWith("camera-sound")) {
             this.moreInfo(this.cc.soundId)
         } else if (id.startsWith("camera-battery")) {
